ebc93fedf0a425af30129fe2e7289d3362c3a411
momoogles <acmomoogles777@gmail.com>
2024-07-31 07:57:38 +0900

    ⚡️

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
   output: "export",
+  assetPrefix: isProd
+    ? "https://mo-repros.github.io/nextjs-tailwindcss-grid-layout-shift/"
+    : "",
 }
 
 export default nextConfig
